12c9b4cb6b50bc54c9ae75e352ddad7d56a1e25e
yasu9780 <kazu.yasutake@gmail.com>
2017-01-11 13:19:12 +0900

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    GameObject obj;
+    GameObject obj2;
+    void Start () {
+        obj = GameObject.Find("Cube");
+        obj2 = GameObject.Find("Cube2");
+    }
+
+    void Update () {
+        obj.transform.Rotate(Vector3.up*Time.deltaTime*20f );
+        obj2.transform.Rotate(Vector3.right * Time.deltaTime * 20f);
+    }
+}
